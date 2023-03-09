eb0436ecd3dcef125dc42a7ee90f04957e649a5e
LeonardoVegaAnanda <121258280+LeonardoVegaAnanda@users.noreply.github.com>
2023-03-09 17:49:15 -0600

    Agregando Service a SP

---
+++ Eternity1.0/Eternity1.0/Controllers/ItemController.cs
@@ -0,0 +1,154 @@
+﻿using Eternity1._0.Models;
+using Eternity1._0.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using RestSharp;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eternity1._0.Models.Items;
+
+namespace Eternity1._0.Controllers
+{
+
+
+    [ApiController]
+    public class ItemController : Controller
+    {
+        string sessionId;
+        private readonly string cadenaSQL;
+
+
+        LoginService service = new LoginService();
+        ItemUbicacionesService itemService = new ItemUbicacionesService();
+
+        public ItemController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
+        [Route("ananda/eternity/ItemTales")]
+        [HttpGet]
+        public IActionResult findItemByItemCodeTales(string itemCode)
+        {
+            var cliente = new RestClient("https://199.89.53.35:50000/b1s/v1/Items" + "('" + itemCode + "')");
+            cliente.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Cookie", "B1SESSION=" + service.LoginSAP());
+            var body = @"";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = cliente.Execute(request);
+            var resultado = JsonConvert.DeserializeObject<Item>(response.Content, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            ItemUbicaciones item = new ItemUbicaciones(resultado.ItemCode,resultado.ItemName,resultado.BarCode,resultado.SalesUnitWeight,resultado.ItemsGroupCode,resultado.U_codigo,itemService.listarUbicaciones(itemCode));
+            
+            if (item.itemCode != null)
+            {
+                List<UbicacionesItems> lista = new List<UbicacionesItems>();
+                List<UbicacionesItems> listaUbis = new List<UbicacionesItems>();
+
+                try
+                {
+                    using (var conexion = new SqlConnection(cadenaSQL))
+                    {
+                        conexion.Open();
+                        var cmd = new SqlCommand("sp_traerUbicaciones", conexion);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        using (var rd = cmd.ExecuteReader())
+                        {
+
+                            while (rd.Read())
+                            {
+                                decimal stockUbicacion;
+                                decimal totalStockAlmacen;
+
+                                if (rd["stockUbicacion"] != DBNull.Value)
+                                {
+                                    stockUbicacion = Convert.ToDecimal(rd["stockUbicacion"]);
+                                }
+                                else
+                                {
+                                    stockUbicacion = 0m;
+                                }
+                                if (rd["TotalStockAlmacen"] != DBNull.Value)
+                                {
+                                    totalStockAlmacen = Convert.ToDecimal(rd["TotalStockAlmacen"]);
+                                }
+                                else
+                                {
+                                    totalStockAlmacen = 0m;
+                                }
+                                lista.Add(new UbicacionesItems
+                                {
+                                    AbsEntry = rd["AbsEntry"].ToString(),
+                                    BinCode = rd["Bincode"].ToString(),
+                                    ItemCode = rd["ItemCode"].ToString(),
+                                    ItemName = rd["ItemName"].ToString(),
+                                    WhsCode = rd["WhsCode"].ToString(),
+                                    StockUbicacion = stockUbicacion,
+                                    TotalStockAlmacen = totalStockAlmacen
+                                });
+                            }
+
+                        }
+
+                    }
+                    listaUbis = lista.Where(item => item.ItemCode == itemCode).ToList();
+                    item.ubicacionesItems = listaUbis;
+                    return StatusCode(StatusCodes.Status200OK,item);
+
+                }
+                catch (Exception error)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, item);
+                }
+            }
+            else
+            {
+                item.itemCode = "0"; ;
+                return StatusCode(StatusCodes.Status200OK, item);
+            }
+
+        }
+
+
+        [HttpGet]
+        [Route("Item")]
+        public IActionResult traerItemByItemCode(string itemCode)
+        {
+            var cliente = new RestClient("https://199.89.53.35:50000/b1s/v1/Items" + "('" + itemCode + "')");
+            cliente.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Cookie", "B1SESSION=" + service.LoginSAP());
+            var body = @"";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = cliente.Execute(request);
+            var resultado = JsonConvert.DeserializeObject<Item>(response.Content, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            Item item = new Item(resultado.ItemCode,resultado.ItemName);
+            if (item.ItemCode != null)
+            {
+                return StatusCode(StatusCodes.Status200OK, item);
+            }
+            else
+            {
+                item.ItemCode = "0"; ;
+                item.ItemName = "";
+                item.NCMCode = "0";
+                item.Properties4 = "";
+                return StatusCode(StatusCodes.Status200OK, item);
+            }
+        }
+    }
+    }
---
+++ Eternity1.0/Eternity1.0/Models/Items/ItemUbicaciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eternity1._0.Models.Items
+{
+    public class ItemUbicaciones
+    {
+        public string itemCode { get; set; }
+        public string itemName { get; set; }
+        public string CodeBars { get; set; }
+        public decimal SWeight1 { get; set; }
+        public int ItmsGrpCod { get; set; }
+        public string U_codigo { get; set; }
+        public List<UbicacionesItems> ubicacionesItems { get; set; }
+
+        public ItemUbicaciones(string itemCode, string itemName, string codeBars, decimal sWeight1, int itmsGrpCod, string u_codigo,List<UbicacionesItems> ubicaciones)
+        {
+            this.itemCode = itemCode;
+            this.itemName = itemName;
+            CodeBars = codeBars;
+            SWeight1 = sWeight1;
+            ItmsGrpCod = itmsGrpCod;
+            U_codigo = u_codigo;
+            this.ubicacionesItems = ubicaciones;
+        }
+    }
+}
